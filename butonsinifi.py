2136024be97ba0825e3f286051b5c0985e2d4df1
arifaltuncu <115743656+arifaltuncu@users.noreply.github.com>
2022-12-17 23:50:09 +0300

    Add files via upload
    Arayüz butonları için buton sınıfı oluşturuldu

---
+++ butonsinifi.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Button():
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
