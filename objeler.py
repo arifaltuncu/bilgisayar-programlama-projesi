42256c675b2189cf387c756c7ab4a27c5a9bafc5
arifaltuncu <115743656+arifaltuncu@users.noreply.github.com>
2022-12-28 19:59:50 +0300

    Add files via upload

---
+++ objeler.py
@@ -0,0 +1,5 @@
+object1 = pygame.Rect(20, 50, 20, 50)
+object2 = pygame.Rect(20, 50, 20, 50)
+object3 = pygame.Rect(20, 50, 20, 50)
+object4 = pygame.Rect(20, 50, 20, 50)
+obstacle = pygame.Rect(350, 500, 800, 50)
