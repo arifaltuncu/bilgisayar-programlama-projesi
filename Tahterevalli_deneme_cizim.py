85d7c30fbfb9e72c4b0e2de6c3d1e2941f5168fc
keremkaptan <115739105+keremkaptan@users.noreply.github.com>
2023-01-19 01:29:17 +0300

    Update Tahterevalli_deneme_cizim.py
    tahterevalli üzerine deneme cisimleri

--- Tahterevalli_deneme_cizim.py
+++ Tahterevalli_deneme_cizim.py
@@ -1,28 +1,30 @@
 import pygame
 
-
 pygame.init()
-display = pygame.display.set_mode((800, 600))
+
+size = (800, 600)
+
+display = pygame.display.set_mode(size)
+
+pygame.display.set_caption("ağırlık")
+
 run = True
+
 siyah = 0 , 0 , 0 # RGB 0- 255
 beyaz = 255 , 255 , 255
 turuncu= 252 , 186 , 3
 yesil= 90 , 180 , 90
 kirmizi = 255,0,0
 
-
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        pygame.draw.rect(display, (kirmizi), pygame.Rect(200, 430, 520, 20),3)
-        pygame.display.flip()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        pygame.draw.polygon(display, (turuncu), [(460,450),(650,550),(300,550)],5)
-        pygame.display.flip()
-
+    display.fill(beyaz)
+    pygame.draw.circle(display, (siyah), (350, 400), 30)
+    pygame.draw.circle(display, (siyah), (550, 390), 40)
+    pygame.draw.rect(display, (kirmizi), pygame.Rect(200, 430, 520, 20),3)
+    pygame.draw.polygon(display, (turuncu), [(460,450),(650,550),(300,550)],5)
+    pygame.display.flip()
 
 pygame.quit()
